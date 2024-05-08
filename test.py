ddd456b87229a241516ceb5f0b392f60ef97af48
James Emilian <dream.jamesemi@gmail.com>
2024-05-08 11:07:56 -0400

    Update test.py

--- test.py
+++ test.py
@@ -13,7 +13,7 @@ ex = Experiment('model_pred', save_git_info=False)
 @ex.config
 def my_config():
     e = 29 # the model checkpoint at epoch e
-    train_exp_num = 1 # the training experiment number
+    train_exp_num = 5 # the training experiment number
     train_exp_dir = './results/%d'%train_exp_num # training experiment directory
     time_interval = 30 # get rppg for 30s video clips, too long clips might cause out of memory
 
@@ -53,8 +53,6 @@ def my_main(_run, e, train_exp_dir, device, time_interval):
         return rppg
 
     for h5_path in test_list:
-        h5_path = str(h5_path)
-
         with h5py.File(h5_path, 'r') as f:
             imgs = f['imgs']
             bvp = f['bvp']
@@ -68,16 +66,28 @@ def my_main(_run, e, train_exp_dir, device, time_interval):
             bvp_list = []
             # bvppeak_list = []
 
+            # Derive the subject number from the h5 filename
+            subject_num = os.path.basename(h5_path).split('.')[0]
+            ground_truth_path = f'/data-fast/james/triage/datasets/UBFC-rPPG/subject{subject_num}/ground_truth.txt'
+            if not os.path.exists(ground_truth_path):
+                print(f"Ground truth file missing for subject{subject_num}, skipping...")
+                continue
+
+            with open(ground_truth_path, 'r') as file:
+                bvp_data = np.array([float(line.strip()) for line in file])
+
             for b in range(num_blocks):
                 rppg_clip = dl_model(imgs[b*time_interval*fs:(b+1)*time_interval*fs])
                 rppg_list.append(rppg_clip)
-
-                bvp_list.append(bvp[b*time_interval*fs:(b+1)*time_interval*fs])
-                # bvppeak_list.append(bvppeak[b*time_interval*fs:(b+1)*time_interval*fs])
-
-            rppg_list = np.array(rppg_list)
-            bvp_list = np.array(bvp_list)
+                bvp_list.append(bvp_data[b*time_interval*fs:(b+1)*time_interval*fs])
             # bvppeak_list = np.array(bvppeak_list)
             # results = {'rppg_list': rppg_list, 'bvp_list': bvp_list, 'bvppeak_list':bvppeak_list}
-            results = {'rppg_list': rppg_list, 'bvp_list': bvp_list}
-            np.save(pred_exp_dir+'/'+h5_path.split('/')[-1][:-3], results)
+            
+            results = {'rppg_list': np.array(rppg_list), 'bvp_list': np.array(bvp_list)}
+            np.save(pred_exp_dir + '/' + os.path.basename(h5_path).replace('.h5', ''), results)
+
+            # Save results back to the same location as the original .h5 file
+            results = {'rppg_list': np.array(rppg_list), 'bvp_list': np.array(bvp_list)}
+            with h5py.File(h5_path, 'w') as result_file:  # Open the same file for writing
+                for key in results:
+                    result_file.create_dataset(key, data=results[key])
